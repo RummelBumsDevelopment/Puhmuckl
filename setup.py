23a1c2a4ff8e1853177c9178fa17b8809d9ade2d
krippix <krippix@users.noreply.github.com>
2022-02-05 15:39:02 +0100

    docker testing

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+   name='puhmuckl',
+   version='0.1',
+   description='Discord BOT Puhmuckl',
+   packages=['puhmuckl'],
+   install_requires=['discord.py'], #external packages as dependencies
+)
