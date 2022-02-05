e74ae1963d445bd8a3b002ce0d7bb09e90b952f9
Lauchmelder <robert.trololo@gmail.com>
2022-02-05 16:22:41 +0100

    use logging module

--- puhmuckl/bot.py
+++ puhmuckl/bot.py
@@ -1,21 +1,24 @@
 from settings import BOT_PREFIX, BOT_TOKEN
 import os
+import logging
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=BOT_PREFIX, case_insensitive=True)
+pwd = os.path.dirname(__file__)
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f"We have logged in as {bot.user}")
 
-    for cog in os.listdir("./cogs"):
+    for cog in os.listdir(os.path.join(pwd, "./cogs")):
         if cog.endswith(".py"):
             cog_name = cog[:-3]
             try:
                 bot.load_extension(f"cogs.{cog_name}")
-                print(f"Loaded module {cog_name}")
+                logging.info(f"Loaded module {cog_name}")
             except Exception as e:
-                print(f"Failed to load module {cog_name}")
+                logging.error(f"Failed to load module {cog_name}. Continuing")
                 pass
 
 bot.run(BOT_TOKEN)
