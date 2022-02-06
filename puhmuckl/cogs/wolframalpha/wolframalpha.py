b771fdfc0ec8133cb665fe225c20e2646cc36706
Lauchmelder <robert.trololo@gmail.com>
2022-02-07 00:25:19 +0100

    added debug embed

--- puhmuckl/cogs/wolframalpha/wolframalpha.py
+++ puhmuckl/cogs/wolframalpha/wolframalpha.py
@@ -1,3 +1,4 @@
+from pydoc import describe
 import discord
 from discord.ext import commands
 from cogs.wolframalpha.wolframapi import WolframResponse
@@ -8,10 +9,11 @@ class TestEmbed(interactive.InteractiveEmbed):
         "close": "❌"
     }
 
-    def __init__(self, parent, ctx):
+    def __init__(self, parent, ctx, message):
         super(TestEmbed, self).__init__(parent.bot, ctx, 60.0)
         self.parent = parent
         self.owner = self.ctx.author
+        self.content = message
 
     async def add_navigation(self, message):
         await message.add_reaction(TestEmbed.REACTIONS["close"])
@@ -25,8 +27,8 @@ class TestEmbed(interactive.InteractiveEmbed):
 
     def make_embed(self):
         embed = discord.Embed(
-            title = "Wolfram|Alpha Test",
-            description = "This is a test embed. You can close it by reacting with ❌",
+            title = "First 100 characters of response",
+            description = f"```{self.content}```",
             color = 0xff7e00
         )
 
@@ -41,4 +43,5 @@ class WolframAlpha(commands.Cog):
     @commands.command(name="wolframalpha", description="Queries Wolfram|Alpha", usage="<query>", aliases=["wolf", "wa"])
     async def wolframalpha(self, ctx, *query):
         response = WolframResponse(" ".join(query))
-        await ctx.send("Queried request. Check logs.")
+        self.activeObjects[ctx.channel.id] = TestEmbed(self, ctx, response.response.text[0:100])
+        await self.activeObjects[ctx.channel.id].show_embed()
