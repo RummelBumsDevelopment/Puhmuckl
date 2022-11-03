2703f014302918950801cda82fa5aa2cfe798abe
krippix <krippix@user.noreply.github.com>
2022-11-03 12:42:41 +0100

    schlimme dinge

--- puhmuckl/util/other.py
+++ puhmuckl/util/other.py
@@ -3,7 +3,9 @@ import re
 def is_emoji(word: str) -> bool:
     '''Checks if string is a discord emoji'''
 
-    if re.search("<a?:.+?:\d{18}>", word):
+    if re.search("<a?:.+?:\d{18}>", word) is not None:
+        print(f"{word} is an emoji")
         return True
     else:
+        print(f"{word} is NOT an emoji")
         return False
